211162d3aab6ba6962dcf985fa293926ec19db96
ilikecheesybread <53441990+ilikecheesybread@users.noreply.github.com>
2020-09-08 21:02:03 +0100

    Initial commit

---
+++ script.py
@@ -0,0 +1,7 @@
+import pyautogui, time
+print("STARTING IN 15 SECONDS! SWITCH TO WHATSAPP WEB!")
+time.sleep(15)
+f = open("beemovescript.txt", 'r')
+for word in f:
+ pyautogui.typewrite(word)
+ pyautogui.press("enter")
